0fdb728108ae7ba386090b0a60cae6694551652e
m8loot <tazorbeatsss@gmail.com>
2023-03-06 22:06:11 +0300

    ss

---
+++ src/js/01_main.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('.index-slider__wrapper-bg').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows:true,
+    dots:true
+  })
+});
